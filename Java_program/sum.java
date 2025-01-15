bea1a4f353c1eb61e65341fa6c2a6c6bcdac9118
Shrivathsa Shettigar <shrivaths@gmail.com>
2025-01-15 09:51:45 +0530

    1 file created

---
+++ Java_program/sum.java
@@ -0,0 +1,12 @@
+class Sum
+{
+  public static void main(String args[])
+   {
+     int x,y,sum;
+     int x=5;
+     int y=6;
+     sum=x+y;
+     System.out.println("Sum of two no is",+sum);
+   }
+}
+   
